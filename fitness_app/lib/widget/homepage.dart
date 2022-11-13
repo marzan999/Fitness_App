674da8e83e8b5f35a961d9de30c65c16ceb6ac41
Marzan Islam <marzanislam999@gmail.com>
2022-11-13 21:31:11 +0600

    Update homepage.dart

--- fitness_app/lib/widget/homepage.dart
+++ fitness_app/lib/widget/homepage.dart
@@ -47,11 +47,126 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 172, 168, 137),
+          title: Text(
+            'Fit your body',
+            style: TextStyle(
+                fontSize: 25, color: Colors.black, fontWeight: FontWeight.bold),
+          ),
+          centerTitle: true,
+        ),
         body: ListView.builder(
             itemCount: allData.length,
             shrinkWrap: true,
             itemBuilder: (context, index) {
-              return Text('${allData[index].id}');
+              // return Text('${allData[index].id}');
+              return Container(
+                height: 1000,
+                child: ListView.separated(
+                    itemBuilder: (context, index) => Container(
+                          height: 450,
+                          color: Color.fromARGB(255, 212, 209, 209),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              children: [
+                                Row(
+                                  children: [
+                                    Container(
+                                      height: 50,
+                                      width: 50,
+                                      decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(30),
+                                          color: Colors.grey),
+                                      child: Center(
+                                        child: Text(
+                                          '${allData[index].id}',
+                                          style: TextStyle(
+                                              fontSize: 25,
+                                              color: Colors.black,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      width: 15,
+                                    ),
+                                    Text(
+                                      '${allData[index].title}',
+                                      style: TextStyle(
+                                          fontSize: 25,
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ],
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Row(
+                                    children: [
+                                      Container(
+                                        height: 150,
+                                        width: 280,
+                                        decoration: BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(20),
+                                            image: DecorationImage(
+                                                image: NetworkImage(
+                                                    '${allData[index].thumbnail}'),
+                                                fit: BoxFit.cover)),
+                                      ),
+                                      SizedBox(
+                                        width: 10,
+                                      ),
+                                      Text(
+                                        'You have\nto do this\nexercise\n${allData[index].seconds} times\nat a time',
+                                        style: TextStyle(fontSize: 25),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                                Align(
+                                    alignment: Alignment.topLeft,
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(
+                                        'As example:',
+                                        style: TextStyle(fontSize: 18),
+                                      ),
+                                    )),
+                                SizedBox(
+                                  height: 5,
+                                ),
+                                // Container(
+                                //   height: 150,
+                                //   width: 280,
+                                //   color: Colors.yellow,
+                                //   child: Image.network('${allData[index].gif}'),
+                                // ),
+                                Align(
+                                  alignment: Alignment.bottomRight,
+                                  child: Container(
+                                    height: 170,
+                                    width: 330,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(20),
+                                        image: DecorationImage(
+                                            image: NetworkImage(
+                                                '${allData[index].gif}'),
+                                            fit: BoxFit.cover)),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                    separatorBuilder: (_, index) => SizedBox(
+                          height: 10,
+                        ),
+                    itemCount: allData.length),
+              );
             }),
       ),
     );
