1e64b73e03b1353ba485d55de900264533280880
Marzan Islam <marzanislam999@gmail.com>
2022-11-14 20:33:53 +0600

    .

---
+++ fitness_app/lib/widget/thumbnail.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ThumbnailPage extends StatefulWidget {
+  const ThumbnailPage({super.key});
+
+  @override
+  State<ThumbnailPage> createState() => _ThumbnailPageState();
+}
+
+class _ThumbnailPageState extends State<ThumbnailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+    );
+  }
+}
